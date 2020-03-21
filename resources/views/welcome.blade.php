8ef7fe809de66d4f204e94c9d07938db291fcc5f
Maxence <maxence.lux@supinfo.com>
2020-03-21 12:44:10 +0100

    fix footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,12 @@
 
         <title>Laravel</title>
 
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Crimson+Text|Work+Sans:400,700" rel="stylesheet">
@@ -48,7 +54,7 @@
             }
 
             .title {
-                font-size: 75px;
+                font-size: 5px;
             }
 
             .links > a {
@@ -61,12 +67,13 @@
                 text-transform: uppercase;
             }
 
-            h2 {
+            h1 {
                 font-family: 'Crimson Text', serif;
+                font-size: 80px;
             }
 
             .between {
-                margin-bottom: 90px;
+                margin-bottom: 80px;
             }
 
             .desc p {
@@ -81,8 +88,13 @@
                 width: 100%;
                 height: 10%;
                 opacity: 0.45;
+                text-align: center;
             }
 
+            .footer p {
+                color: white;
+                font-size: 22px;
+            }
 
         </style>
     </head>
@@ -104,7 +116,7 @@
 
             <div class="content">
                 <div class="title between">
-                    <h2> Todo List </h2>
+                    <h1> Todo List </h1>
                 </div>
 
                 <div class="desc">
@@ -113,8 +125,18 @@
 
             </div>
         </div>
-        <div class="footer">
-
+        <div class="container-fluid footer">
+            <div class="row">
+                <div class="col">
+                    <p>One of three columns </p>
+                </div>
+                <div class="col">
+                    <p>One of three columns </p>
+                </div>
+                <div class="col">
+                    <p>One of three columns </p>
+                </div>
+            </div>
         </div>
 
     </body>
