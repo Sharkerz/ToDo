910f831ed38144546afadef7aead358be937340b
Maxence <maxence.lux@supinfo.com>
2020-04-02 01:12:51 +0200

    fix before go sleep

--- public/js/Amis.js
+++ public/js/Amis.js
@@ -1,18 +1,18 @@
 /* Supprimer un ami */
 $(document).ready(function () {
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms))
-    }
-
-    async function alert() {
-        await sleep(2000);
-        document.getElementById('alert_success_friend').setAttribute('visibility', 'visible');
-        $('.alert').alert();
-        await sleep(5000);
-        $('.alert').alert('close');
-    }
-    alert();
+    // function sleep(ms) {
+    //     return new Promise(resolve => setTimeout(resolve, ms))
+    // }
+    //
+    // async function alert() {
+    //     await sleep(2000);
+    //     document.getElementById('alert_success_friend').setAttribute('visibility', 'visible');
+    //     $('.alert').alert();
+    //     await sleep(5000);
+    //     $('.alert').alert('close');
+    // }
+    // alert();
 
     $('#confirm_delete_friend').click(function () {
         $.ajax({
